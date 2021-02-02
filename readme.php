0fe6eda8a25cb64c2058bf419a74cafba618a5e6
Anna van Biemen <annavanbiemen@gmail.com>
2021-02-02 10:52:26 +0100

    Less comments in readme.php.

--- readme.php
+++ readme.php
@@ -89,45 +89,22 @@ class Appointment
 
     public function creator(): Neat\Object\Relations\One
     {
-        // Pass reference configuration to belongsToOne as
-        // callable(LocalKeyBuilder)
         return $this->belongsToOne(User::class, 'creator', function (Neat\Object\Relations\Reference\LocalKeyBuilder $builder) {
-            // Use the local property name
-            $builder->setLocalKey('createdBy');
-
-            // Or alternatively, the local column name
-            $builder->setLocalKeyColumn('created_by');
-
-            // Set the remote property name
-            $builder->setRemoteKey('alternativeId');
-
-            // Or alternatively, the remote column name
-            $builder->setRemoteKeyColumn('alternative_id');
+            // ...
         });
     }
 
     public function agendaLines(): Neat\Object\Relations\Many
     {
-        // Pass reference configuration to hasOne and hasMany as
-        // callable(RemoteKeyBuilder)
         return $this->hasMany(AgendaLine::class, 'agenda', function (Neat\Object\Relations\Reference\RemoteKeyBuilder $builder) {
-            // The same local and remote key setters as with belongsToOne
-            // can be used with hasMany and hasOne relations.
+            // ...
         });
     }
 
     public function attendees(): Neat\Object\Relations\Many
     {
-        // Pass reference configuration to belongsToMany as
-        // callable(JunctionTableBuilder)
         return $this->belongsToMany(User::class, 'attendees', function (Neat\Object\Relations\Reference\JunctionTableBuilder $builder) {
-            // Set the junction table name and column names in addition to
-            // the same local and remote key setters as with belongsToOne.
-            $builder->setJunctionTable('appointment_attendee');
-            $builder->setJunctionTableLocalKeyColumn('appointment_id');
-            $builder->setJunctionTableRemoteKeyColumn('attendee_id');
-            // Please note that the junction table doesn't have an entity
-            // class. Therefore you cannot use class and property names.
+            // ...
         });
     }
 }
